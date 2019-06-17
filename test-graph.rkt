ff76f7dd67e28284ae28176ede225bb0af4875a4
Stefan kruger <stefan.kruger@gmail.com>
2019-06-17 15:14:19 +0100

    test for prim-mst

--- test-graph.rkt
+++ test-graph.rkt
@@ -4,6 +4,12 @@
 (require rackunit)
 (require rackunit/text-ui)
 
+(define (mhash . xs)
+  (let ([ht (make-hash)])
+    (for ([pair (in-slice 2 xs)])
+      (apply hash-set! ht pair))
+    ht))
+
 (define graph-tests
   (test-suite
    "Tests for dijkstra [graph.rkt]"
@@ -41,29 +47,28 @@
               'F (hash 'A 16 'E 11))])
       (check-equal? (unwind-path (dijkstra g #:start 'A #:end 'E) 'A 'E) (list 'A 'C 'D 'E))))
 
-;   (test-case
-;    "Weighted graph"
-;    (let* ([g (hash
-;              'A (hash 'B 4 'H 8)
-;              'B (hash 'A 4 'C 8 'H 11)
-;              'C (hash 'B 8 'D 7 'F 4 'I 2)
-;              'D (hash 'C 7 'E 9 'F 14)
-;              'E (hash 'D 9 'F 10)
-;              'F (hash 'C 4 'D 14 'E 10 'G 2)
-;              'G (hash 'F 2 'H 1 'I 6)
-;              'H (hash 'A 8 'B 11 'G 1 'I 7)
-;              'I (hash 'C 2 'H 7 'G 6)
-;              )]
-;          [expected (hash
-;                     'A (mutable-set 'H 'B)
-;                     'C (mutable-set 'D 'I)
-;                     'D (mutable-set 'E)
-;                     'F (mutable-set 'C)
-;                     'G (mutable-set 'F)
-;                     'H (mutable-set 'G))]
-;          [actual (prim-mst g 'A)]
-;          [is-equal (equal? actual expected)])
-;      (check-equal? is-equal #t)))))
-   ))
+   (test-case
+    "Weighted graph"
+    (let* ([g (hash
+              'A (hash 'B 4 'H 8)
+              'B (hash 'A 4 'C 8 'H 11)
+              'C (hash 'B 8 'D 7 'F 4 'I 2)
+              'D (hash 'C 7 'E 9 'F 14)
+              'E (hash 'D 9 'F 10)
+              'F (hash 'C 4 'D 14 'E 10 'G 2)
+              'G (hash 'F 2 'H 1 'I 6)
+              'H (hash 'A 8 'B 11 'G 1 'I 7)
+              'I (hash 'C 2 'H 7 'G 6)
+              )]
+          [expected (mhash
+                     'A (mutable-set 'H 'B)
+                     'C (mutable-set 'D 'I)
+                     'D (mutable-set 'E)
+                     'F (mutable-set 'C)
+                     'G (mutable-set 'F)
+                     'H (mutable-set 'G))]
+          [actual (prim-mst g 'A)]
+          [is-equal (equal? actual expected)])
+      (check-equal? is-equal #t)))))
 
 (run-tests graph-tests)
