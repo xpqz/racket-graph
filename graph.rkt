6fe653daa55e69a58a102f1ec1f6ab4e627c122e
Stefan kruger <stefan.kruger@gmail.com>
2019-06-21 09:37:08 +0100

    Store edges and adjacency separately

--- graph.rkt
+++ graph.rkt
@@ -1,6 +1,6 @@
 #lang racket
 ;;
-;; Naive graph stuff for Racket
+;; Naive undirected graph stuff for Racket
 ;;
 ;; Stefan Kruger (c) 2019
 ;;
@@ -24,28 +24,40 @@
   (λ (start end #:weight [weight 0])
     (edge start end weight)))
 
-(struct graph ([nodes #:mutable] [edges #:mutable]) #:transparent)
+(struct graph ([nodes #:mutable] [edges #:mutable] [adjacency #:mutable]) #:transparent)
 
 (define (add-edge! g edge)
   (let ([start (edge-start edge)]
         [end (edge-end edge)]
         [weight (edge-weight edge)]
-        [edges (graph-edges g)])
+        [edges (graph-edges g)]
+        [adj (graph-adjacency g)])
     (for ([n (list start end)])
-      (unless (set-member? (graph-nodes g) n) (set-add! (graph-nodes g) n)))
-    
-    (cond [(not (hash-has-key? edges start))
-           (hash-set! edges start (make-hash (list (cons end weight))))]
-          [else
-           (hash-set! (hash-ref edges start) end weight)])
+      (set-add! (graph-nodes g) n))
 
-    (cond [(not (hash-has-key? edges end))
-           (hash-set! edges end (make-hash (list (cons start weight))))]
+    ;; start-end
+    (cond [(not (hash-has-key? adj start))
+           (hash-set! adj start (mutable-set end))]
           [else
-           (hash-set! (hash-ref edges end) start weight)])))
+           (set-add! (hash-ref adj start) end)])
+
+    ;; end-start
+    (cond [(not (hash-has-key? adj end))
+           (hash-set! adj end (mutable-set start))]
+          [else
+           (set-add! (hash-ref adj end) start)])
+
+    ;; edge
+    (hash-set! edges (set start end) weight)))
+
+(define (graph-edge-cost graph vset)
+  (hash-ref (graph-edges graph) vset))
+
+(define (graph-neighbours graph node)
+  (hash-ref (graph-adjacency graph) node))
 
 (define (make-graph . xs)
-  (let ([g (graph (mutable-set) (make-hash))])
+  (let ([g (graph (mutable-set) (make-hash) (make-hash))])
     (for ([edge xs])
       (add-edge! g edge))
     g))
@@ -57,8 +69,8 @@
            (loop (hash-ref came-from current) (cons current path))])))
 
 (define (frontier-nodes graph cost-so-far current)
-  (for*/list ([(elem cost) (in-hash (hash-ref (graph-edges graph) current))]
-              [new-cost (in-value (+ (hash-ref cost-so-far current) cost))]
+  (for*/list ([elem (in-set (graph-neighbours graph current))]
+              [new-cost (in-value (+ (hash-ref cost-so-far current) (graph-edge-cost graph (set current elem))))]
               #:when (or
                       (not (hash-has-key? cost-so-far elem))
                       (< new-cost (hash-ref cost-so-far elem))))
@@ -90,8 +102,8 @@
   ;;
   ;; See https://en.wikipedia.org/wiki/Prim%27s_algorithm
   (let ([edges (make-heap-queue)])
-    (for ([(to cost) (in-hash (hash-ref (graph-edges graph) start))])
-      (push-heap-queue! edges cost (cons start to)))
+    (for ([to (in-set (graph-neighbours graph start))])
+      (push-heap-queue! edges (graph-edge-cost graph (set start to)) (cons start to)))
     
     (let loop ([mst (make-hash)] [edges edges] [visited (mutable-set start)])
       (cond [(heap-queue-empty? edges) mst]
@@ -106,9 +118,9 @@
                    (hash-set! mst from (mutable-set)))
                  (set-add! (hash-ref mst from) to))
 
-               (for ([(next cost) (in-hash (hash-ref (graph-edges graph) to))])
+               (for ([next (in-set (graph-neighbours graph to))])
                  (unless (set-member? visited next)
-                   (push-heap-queue! edges cost (cons to next))))
+                   (push-heap-queue! edges (graph-edge-cost graph (set to next)) (cons to next))))
                
                (loop mst edges visited))]))))
                           
@@ -132,7 +144,7 @@
                (let ([current (dequeue! frontier)])
                  (cond [(and (not (null? end)) (equal? current end)) came-from]
                        [else
-                        (for ([neighbour (hash-keys (hash-ref (graph-edges graph) current))])
+                        (for ([neighbour (in-set (graph-neighbours graph current))])
                           (unless (set-member? visited neighbour)
                             (enqueue! frontier neighbour)
                             (hash-set! came-from neighbour current)
